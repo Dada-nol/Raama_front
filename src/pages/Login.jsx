6c3b32e26c926e3f8c6717ebf654fde29e79c403
Darren <nardol.darren@gmail.com>
2025-06-20 16:42:04 +0200

    Add login method

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,9 +1,45 @@
-import React from "react";
+import axios from "axios";
+import { useState } from "react";
 
 function Login() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleLogin = async (e) => {
+    e.preventDefault();
+
+    const res = await axios.post("http://localhost:8000/api/login", {
+      email,
+      password,
+    });
+
+    localStorage.setItem("token", res.data.token);
+    alert("Connecté !");
+  };
   return (
-    <form>
-      <input type="text" />
+    <form className="form" onSubmit={handleLogin}>
+      <div className="form-item">
+        <div>
+          <label htmlFor="">Email</label>
+        </div>
+        <input
+          type="text"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+      </div>
+      <div className="form-item">
+        <div>
+          <label htmlFor="">Password</label>
+        </div>
+        <input
+          type="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+      </div>
+
+      <button type="submit">Se connecter</button>
     </form>
   );
 }
