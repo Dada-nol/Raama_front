c728db2e40e340eba3023e3f9a2775d9b2bd39b7
Darren <nardol.darren@gmail.com>
2025-07-24 22:55:44 +0200

    add home page content

--- src/components/layouts/main/Layout.jsx
+++ src/components/layouts/main/Layout.jsx
@@ -1,12 +1,18 @@
 import { Outlet } from "react-router-dom";
-import NavBar from "./BurgerButton";
+import SideBar from "./SideBar";
 // import Footer from "./Footer";
 
 function Layout() {
   return (
     <>
-      <NavBar />
+      <SideBar />
       <main className="flex-1 overflow-y-auto text-center">
+        <div className="flex items-center justify-between p-4 mb-4">
+          <h1 className="text-gradient w-fit text-[26px]">Raama</h1>
+          <div className="w-10 h-10 border rounded-[50px]">
+            <button className="p-2">?</button>
+          </div>
+        </div>
         <Outlet />
       </main>
       {/* <Footer /> */}
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,8 +4,28 @@ function Home() {
   const { user } = useAuth();
   return (
     <>
-      <div>
-        <h1 className="text-gradient">Bienvenue {user.firstname} !</h1>
+      <h2 className="flex justify-center items-center">
+        Bienvenue {user.firstname} !
+      </h2>
+
+      <div className="border-2 border-primary m-5">
+        <h3 className="flex justify-start p-4">Recent</h3>
+
+        <ul className="flex justify-between items-center p-4">
+          <li>1</li>
+          <li>2</li>
+          <li>3</li>
+        </ul>
+      </div>
+
+      <div className="border-2 border-primary m-5">
+        <h3 className="flex justify-start p-4">Créer de nouveau souvenirs</h3>
+
+        <ul className="flex justify-between items-center p-4">
+          <li>1</li>
+          <li>2</li>
+          <li>3</li>
+        </ul>
       </div>
 
       <div>{/* <Souvenirs></Souvenirs> */}</div>
