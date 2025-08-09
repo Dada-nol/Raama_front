924eb45d18bb2186898d0af9b833d613f41f2472
Nardol Darren <nardol.darren@gmail.com>
2025-08-09 21:12:15 +0200

    add social icon link

--- src/components/ui/SocialIcons.jsx
+++ src/components/ui/SocialIcons.jsx
@@ -5,14 +5,16 @@ import Icon from "./Icon";
 
 function SocialIcons() {
   const socialIcons = [
-    { src: facebookIcon, alt: "Facebook" },
-    { src: instagramIcon, alt: "Instagram" },
-    { src: linkedinIcon, alt: "LinkedIn" },
+    { href: "https://facebook.com", src: facebookIcon, alt: "Facebook" },
+    { href: "https://instagram.com", src: instagramIcon, alt: "Instagram" },
+    { href: "https://linkedin.com", src: linkedinIcon, alt: "LinkedIn" },
   ];
   return (
     <div className="flex justify-center items-center gap-2">
       {socialIcons.map((icon, i) => (
-        <Icon key={i} src={icon.src} alt={icon.alt} />
+        <a rel="noneferrer noreferrer" target="_blank" href={icon.href}>
+          <Icon key={i} src={icon.src} alt={icon.alt} />
+        </a>
       ))}
     </div>
   );
---
+++ src/components/ui/SouvenirSetting.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function SouvenirSetting() {
+  return <div>SouvenirSetting</div>;
+}
+
+export default SouvenirSetting;
