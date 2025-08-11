3862b75c0d3782850f4985d782f184bf43754108
Nardol Darren <nardol.darren@gmail.com>
2025-08-11 21:31:57 +0200

    update home tailwind

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -31,64 +31,68 @@ function Home() {
   }, []);
   return (
     <>
-      <h2 className="flex justify-center items-center text-2xl font-medium">
+      <h2 className="flex justify-center items-center text-3xl font-semibold text-text mb-8">
         Bienvenue {user?.firstname} !
       </h2>
 
       <div className=" mx-8">
-        <h3 className="flex justify-start p-4 text-lg font-medium">Recent</h3>
+        <h3 className="flex justify-start px-4 py-2 text-lg font-semibold border-b border-primary mb-6">
+          Recent
+        </h3>
 
-        <ul className="flex justify-start gap-6 items-center mb-8">
+        <ul className="flex flex-wrap gap-6 items-start mb-10">
           {souvenirs.map((souvenir) => (
             <li
               key={souvenir.id}
-              className="card w-80 p-4 relative group overflow-hidden rounded-xl bg-secondary hover:shadow-[0_0_5px_#64b000] transition-transform duration-300 hover:scale-105 border-2 border-primary hover:text-gradient"
+              className="card group w-80 p-4 group overflow-hidden rounded-xl bg-secondary hover:shadow-[0_0_5px_#64b000] transition-transform duration-300 hover:scale-105 border-2 border-primary hover:text-gradient"
             >
               <a href={`souvenir/${souvenir.id}`}>
-                {souvenir.cover_image ? (
-                  <img
-                    src={
-                      souvenir.cover_image
-                        ? `http://localhost:8000/storage/${souvenir.cover_image}`
-                        : "none"
-                    }
-                    alt="souvenir"
-                    className="w-full h-48 object-cover rounded-lg"
-                  />
-                ) : (
-                  <img
-                    src={panda3}
-                    alt="panda dans un arbre"
-                    className="w-full h-48 object-cover rounded-lg"
-                  />
-                )}
-                <p className="w-fit">{souvenir.title}</p>
-                <p className="w-fit">{souvenir.memory_points}</p>
+                <img
+                  src={
+                    souvenir.cover_image
+                      ? `http://localhost:8000/storage/${souvenir.cover_image}`
+                      : panda3
+                  }
+                  alt="souvenir"
+                  className="w-full h-48 object-cover rounded-lg shadow-sm group-hover:shadow-md transition-all"
+                />
+                <div className="mt-3">
+                  <p className="text-lg font-medium group-hover:text-gradient transition-colors">
+                    {souvenir.title}
+                  </p>
+                  <p className="text-sm text-gray-300">
+                    {souvenir.memory_points} points
+                  </p>
+                </div>
               </a>
             </li>
           ))}
         </ul>
       </div>
 
-      <div className="border-2 border-primary mx-8">
-        <h3 className="flex justify-start p-4 text-lg font-medium">
+      {/* Section Créer souvenirs */}
+      <div className="border-2 border-primary mx-8 rounded-xl shadow-md">
+        <h3 className="flex items-center p-4 text-lg font-semibold border-b border-primary">
           Créer de nouveaux <p className="text-gradient pl-1">souvenirs</p>
         </h3>
 
-        <ul className="flex justify-between items-center p-4 gap-6">
+        <ul className="flex flex-wrap justify-start p-6 gap-6">
           {memoryType.map((memory) =>
             memory.isAvailable ? (
               <li
                 key={memory.id}
-                className="card w-80 p-4 relative group overflow-hidden rounded-xl bg-secondary gradient-border transition-transform duration-300 hover:scale-105 hover:text-gradient"
+                className="card w-80 p-4 shadow-md hover:shadow-lg group overflow-hidden  bg-secondary gradient-border transition-transform duration-300 hover:scale-105 hover:text-gradient"
               >
-                <a href={`souvenir/create?memory_type_id=${memory.id}`}>
+                <a
+                  className="flex items-center justify-center w-full h-full block text-lg font-medium text-text hover:text-gradient transition-colors"
+                  href={`souvenir/create?memory_type_id=${memory.id}`}
+                >
                   {memory.title}
                 </a>
               </li>
             ) : (
               <li className="cursor-not-allowed w-80 h-28 p-4 relative overflow-hidden bg-secondary border-2 border-danger opacity-60">
-                <div className="absolute inset-0 bg-black/40 flex flex-col items-center justify-center gap-2">
+                <div className="absolute inset-0 bg-black/40 flex flex-col items-center justify-center gap-1">
                   <LockClosedIcon className="w-6 h-6 text-white" />
                   <p className="text-white font-bold">{memory.title}</p>
                   <p className="text-sm text-gray-200">
@@ -100,8 +104,6 @@ function Home() {
           )}
         </ul>
       </div>
-
-      <div>{/* <Souvenirs></Souvenirs> */}</div>
     </>
   );
 }
